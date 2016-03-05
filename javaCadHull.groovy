43dbef8b17dc3193df7c330f07a79347a90debc6
madhephaestus <mad.hephaestus@gmail.com>
2016-03-05 18:01:50 -0500

    updated hull demo/n/n

--- javaCadHull.groovy
+++ javaCadHull.groovy
@@ -4,14 +4,10 @@ CSG cube = new Cube(	size,// X dimention
 			size,// Y dimention
 			size//  Z dimention
 			).toCSG()
-//cubeIntersectSphere = cubeIntersectSphere.move(1,2,3);// vector notation
 CSG movedCube =  new Sphere(size).toCSG()
 			.movex(20)
 			.movey(40)
 			.movez(60)
-
-//set colors
-
 //This section is how you for a shape of the "shrinkwrap" of the 2 shapes. 
 CSG hulledCubes = cube.union(movedCube).hull();
 

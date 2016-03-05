84f4e2e2d1476a5d0095dc5ac45c486a1ea5b40e
madhephaestus <mad.hephaestus@gmail.com>
2016-03-05 14:19:48 -0500

    better hull example/n/n

--- javaCadHull.groovy
+++ javaCadHull.groovy
@@ -5,16 +5,11 @@ CSG cube = new Cube(	size,// X dimention
 			size//  Z dimention
 			).toCSG()
 //cubeIntersectSphere = cubeIntersectSphere.move(1,2,3);// vector notation
-CSG movedCube = cube
-			.movex(10)
-			.movey(20)
-			.movez(30)
-//rotate
-//cubeIntersectSphere = cubeIntersectSphere.rot(15,20,30);// vector notation
-movedCube = movedCube
-			.rotx(15)
-			.roty(20)
-			.rotz(30)
+CSG movedCube =  new Sphere(size).toCSG()
+			.movex(20)
+			.movey(40)
+			.movez(60)
+
 //set colors
 
 //This section is how you for a shape of the "shrinkwrap" of the 2 shapes. 

43943e293381f5c2b78343a6afab8a31c4cb9b87
Kevin Harrington <mad.hephaestus@gmail.com>
2018-04-19 13:33:41 -0400

    addn on the fly Hull from points

--- javaCadHull.groovy
+++ javaCadHull.groovy
@@ -1,3 +1,6 @@
+import  eu.mihosoft.vrl.v3d.ext.quickhull3d.*
+import eu.mihosoft.vrl.v3d.Vector3d
+
 //Move and rotate opperations
 double size =40;
 CSG cube = new Cube(	size,// X dimention
@@ -13,5 +16,12 @@ CSG hulledCubes = cube.union(movedCube).hull();
 
 //Alternate way to perform a Hull
 hulledCubes = CSG.hullAll([cube,movedCube])
+def points = [	new Vector3d(10, 50, 10),
+			new Vector3d(10, 40, 10),
+			new Vector3d(-10, 50, 10),
+			new Vector3d(-10, 40, 10),
+			new Vector3d(0, 50, 60)
+]
+CSG hullSection = HullUtil.hull(points)
 
-return [cube,movedCube,hulledCubes.movex(size*2)]
+return [cube,movedCube,hulledCubes.movex(size*2),hullSection]
